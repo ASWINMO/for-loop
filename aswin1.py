d4481b70644c9e8484c9f68f8e343f85bd5b4240
ASWINMO <110896795+ASWINMO@users.noreply.github.com>
2022-08-09 15:07:27 +0530

    updated

--- aswin1.py
+++ aswin1.py
@@ -2,8 +2,8 @@ testing------------------
 num=12345
 sum=0
 for i in range(len(str(num))):
-    temp=num%10                                 #num give the values into sum and sum given to temp and check the mode the result
-    num=num//                                    will is 5 and then flordivision the numbers in the sum become 1234 then reapet the steps
+    temp=num%10                                 
+    num=num//                             
     print(num)
     sum+=temp
 print(sum)
